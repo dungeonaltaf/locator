4d8c92a103e37f97d6b6b165e46c79973e696d60
dungeonaltaf <altafsofficial01@gmail.com>
2019-11-04 22:31:49 +0530

    Finally the location showing at exact location.

--- models/gps.js
+++ models/gps.js
@@ -2,18 +2,9 @@ const mongoose = require('mongoose');
 
 const GPSschema = mongoose.Schema({
 
-    latitude :{
-        type: Number,
-        required:true
-    },
-    longitude : {
-        type: Number,
-        required:true
-    },
-    phone : {
-        type: Number,
-
-    }
+    latitude :Number,
+    longitude :Number,
+    phone : Number
 
 });
 
